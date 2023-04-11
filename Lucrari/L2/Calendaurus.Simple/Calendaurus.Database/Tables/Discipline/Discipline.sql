b3f77a5155811a3bc1b5202a987137f6d6f4c0da
Flavius Fudulu <flavius.fudulu@theaccessgroup.com>
2023-04-11 17:16:42 +0300

    for review

---
+++ Lucrari/L2/Calendaurus.Simple/Calendaurus.Database/Tables/Discipline/Discipline.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Discipline]
+(
+	[Id] BIGINT NOT NULL IDENTITY, 
+    [YearId] BIGINT NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    CONSTRAINT [PK_Discipline] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_Discipline_Year] FOREIGN KEY ([YearId]) REFERENCES [Year]([Id]) 
+)
---
+++ Lucrari/L2/Calendaurus.Simple/Calendaurus.Database/Tables/Identity/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] BIGINT NOT NULL IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(320) NOT NULL, 
+    [Address] NVARCHAR(500) NULL, 
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id]), 
+)
