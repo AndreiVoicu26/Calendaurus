c425c349fbb90645c1656cb86971dd61eff2f15c
Flavius Fudulu <flavius.fudulu@theaccessgroup.com>
2023-04-13 19:17:55 +0300

    Added additional tables

---
+++ Lucrari/L2/Calendaurus.Database/Tables/Events/TheoreticalLessonEvent.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[TheoreticalLessonEvent]
+(
+	[Id] BIGINT NOT NULL IDENTITY, 
+    [TheoreticalLessonId] BIGINT NOT NULL, 
+    [ProfessorId] BIGINT NOT NULL, 
+    [DayOfWeek] NVARCHAR(20) NOT NULL, 
+    [StartTime] TIME NOT NULL, 
+    [EndTime] TIME NOT NULL, 
+    [Occurance] TINYINT NULL, 
+    [MaximumSize] TINYINT NULL, 
+    CONSTRAINT [PK_TheoreticalLessonEvent] PRIMARY KEY ([Id]), 
+    CONSTRAINT [FK_TheoreticalLessonEvent_PracticalLesson] FOREIGN KEY ([TheoreticalLessonId]) REFERENCES [TheoreticalLesson]([Id]) 
+)
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'An enum which describes the occurance. ex:1-weekly/2-bi-weekly',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TheoreticalLessonEvent',
+    @level2type = N'COLUMN',
+    @level2name = N'Occurance'
