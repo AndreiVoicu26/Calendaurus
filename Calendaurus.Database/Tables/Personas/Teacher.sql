9f2c51d3bd1e40759eafb16656629fa71745afc9
Andrei <andrei.voicu@student.upt.ro>
2023-04-21 17:21:04 +0300

    Deleted old database

--- Calendaurus.Database/Tables/Personas/Teacher.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Teacher]
-(
-	[Id] BIGINT NOT NULL IDENTITY, 
-    [UserId] BIGINT NOT NULL, 
-    [Title] NVARCHAR(50) NOT NULL, 
-    CONSTRAINT [PK_Teacher] PRIMARY KEY ([Id]), 
-    CONSTRAINT [FK_Teacher_User] FOREIGN KEY ([UserId]) REFERENCES [User]([Id]), 
-    CONSTRAINT [AK_Teacher_UserId] UNIQUE ([UserId]) 
-)
