a2d475d716c2ddc76f4326c3670ef0e372e7cce8
Andrei <andrei.voicu@student.upt.ro>
2023-04-21 17:16:15 +0300

    Deleted old projects

--- andrei.voicu/LigaACLabs/LigaACLabs.Models/Entities/Lab.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LigaACLabs.Models.Entities
-{
-    public class Lab
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public DateTime LabDate { get; set; }
-    }
-}
