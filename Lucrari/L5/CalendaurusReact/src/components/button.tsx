ceb586ccb77e8e71472f23d06a0e64daa6e9c814
Calin Fantaneanu <Calin.Fantaneanu@theaccessgroup.com>
2023-05-01 16:07:17 +0300

    Calendaurus react frontend startup with MS authentication

---
+++ Lucrari/L5/CalendaurusReact/src/components/button.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: 1px solid #8c7cf0;
+  background: rgba(140, 124, 240, 0.24);
+  color: #8c7cf0;
+  cursor: pointer;
+  width: max-content;
+  padding: 0.5rem;
+  border-radius: 5px;
+`;
