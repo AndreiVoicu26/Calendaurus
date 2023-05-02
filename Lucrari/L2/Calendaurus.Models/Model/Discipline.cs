fe89fae7693e64f15c4b8a5e9b0bcb2b34b0886f
Andrei <andrei.voicu@student.upt.ro>
2023-05-02 18:10:24 +0300

    last commit

---
+++ Lucrari/L2/Calendaurus.Models/Model/Discipline.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Calendaurus.Models.Model;
+
+[Table("Discipline")]
+public partial class Discipline
+{
+    [Key]
+    public long Id { get; set; }
+
+    public byte Year { get; set; }
+
+    [StringLength(200)]
+    public string Faculty { get; set; } = null!;
+
+    [StringLength(50)]
+    public string Name { get; set; } = null!;
+
+    [InverseProperty("Discipline")]
+    public virtual ICollection<PracticalLesson> PracticalLessons { get; set; } = new List<PracticalLesson>();
+
+    [InverseProperty("Discipline")]
+    public virtual TheoreticalLesson? TheoreticalLesson { get; set; }
+}
